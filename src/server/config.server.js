3d11742d09b103791be462e8b2db084fad3bbac8
Gabriel <gabrielfernandez090997@gmail.com>
2025-05-15 12:47:02 -0300

    Se hicieron cambios en controladores, rutas y servicios

--- src/server/config.server.js
+++ src/server/config.server.js
@@ -7,8 +7,8 @@ class Server {
   constructor(port) {
     this.port = process.env.PORT || port; // Toma el puerto del sistema o del index.
     this.app = express(); // Se ejecuta express y se crea automaticamente el servidor.
-    /*     this.middlewares();
-    this.routes(); */
+    this.middlewares();
+    this.routes();
   }
 
   middlewares() {
@@ -17,9 +17,9 @@ class Server {
     this.app.use(morgan("dev")); // Registra las peticiones HTTP en la consola.
   }
 
-  /*   routes() {
+  routes() {
     this.app.use("/api", require("../routes/index.routes")); // El api es el identificador de rutas backend.
-  } */
+  }
 
   listen() {
     this.app.listen(process.env.PORT, () => {
